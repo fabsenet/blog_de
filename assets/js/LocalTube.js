f506617a2a40145531a3b0a35c1c667bc55629b5
Fabian Wetzel <fabsenet@googlemail.com>
2019-05-22 17:06:40 +0200

    styling, overlay, ... improved

--- assets/js/LocalTube.js
+++ assets/js/LocalTube.js
@@ -8,11 +8,15 @@ else {
 function replaceThumbnailWithIframe(e) {
     e = e || window.event;
     var target = e.target || e.srcElement;
-    if (target.className.match(/LocalTube-playBtn/)) {
-        parent = target.parentNode;
-        id = parent.getAttribute('data-youtube-id');
-        style = 'style="frameborder=0;"';
-        inner = '<iframe class="youtubePlayer" src="https://www.youtube.com/embed/' + id + '?autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1"' + style + ' />'
-        parent.innerHTML = inner;
+    if (target.className.match(/LocalTubePlayer/)) {
+        const aTag = target;
+        const figureTag = target.parentNode;
+        id = figureTag.getAttribute('data-youtube-id');
+
+        figureTag.removeChild(aTag);
+        const inner = `<iframe class="youtubePlayer" src="https://www.youtube.com/embed/${id}?autoplay=0&autohide=1&border=0&wmode=opaque&enablejsapi=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />`;
+
+        figureTag.innerHTML += inner;
+        e.preventDefault();
     }
 }
